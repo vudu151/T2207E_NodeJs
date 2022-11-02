c92a91e70a64ceee2d73a870c99f565e576f51d2
vudu151 <109599042+vudu151@users.noreply.github.com>
2022-11-02 19:58:06 +0700

    up code

--- index.js
+++ index.js
@@ -1,83 +1,80 @@
-const express = require ("express");      //thuê thợ xây
-const app= express();                    //app là cái nhà
-const PORT = process.env.PORT || 5000;                 //lấy cổng 5000
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 2000;
 
 app.listen(PORT,function () {
-    console.log("Server is running...");
+    console.log("Sever is running...");
 });
 //share api access all
 app.use(function (req,res,next){
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Header","Origin, X-Requested-With, Content-Type, Accept");
-})
-
-
-
-// config to connect mysql
+    res.header("Acccess-Control-Allow-Origin","*");
+    res.header("Acccess-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+// config to connect MySQL
 const configDB = {
     host: "139.180.186.20",
     port: 3306,
     database: "t2207e",
     user: "t2207e",
-    password:"t2207e123", // mamp : "root" -- xampp: ""
-    multipleStatements: true // cho phép sử dụng nhiều câu SQL 1 lần gửi yêu cầu
+    password:"t2207e123", // mamp: "root" --- xampp:"
+    multipleStatements: true // cho phép sử dụng nhiều câu SQL trong 1 lần yêu cầu
+
 };
-// connect to mysql
+
 const mysql = require("mysql");
 const conn = mysql.createConnection(configDB);
 
 // api list all class
-app.get("/get-classes",function (req,res) {
+app.get("/get-classes",function (req,res){
     const sql = "select * from classes";
-    conn.query(sql,function (err,data) {
-        if(err){
+    conn.query(sql, function (err,data){
+        if (err){
             res.send("404 not found");
         }else{
             res.send(data);
         }
     })
 });
-// get all students
-app.get("/get-students",function (req,res) {
+app.get("/get-students",function (req,res){
     const sql = "select * from students";
-    conn.query(sql,function (err,data) {
-        if(err){
+    conn.query(sql, function (err,data){
+        if (err){
             res.send("404 not found");
         }else{
             res.send(data);
         }
     })
 });
-// loc theo cid
-app.get("/student-by-class",function (req,res) {
+//  lọc theo cid
+
+app.get("/student-by-class",function (req,res){
     const cid = req.query.cid;
-    const sql = "select * from students where cid = "+cid;
-    conn.query(sql,function (err,data) {
-        if(err){
+    const sql = "select * from students where cid =" + cid;
+    conn.query(sql, function (err,data){
+        if (err){
             res.send("404 not found");
         }else{
             res.send(data);
         }
     })
 });
-//search by name
-app.get("/search-students",function (req,res) {
+app.get("/student-by-name",function (req,res){
     const q = req.query.q;
-    const sql = `select * from students where name like '%${q}%' or email like '%${q}%'`;
-    conn.query(sql,function (err,data) {
-        if(err){
+    const sql = `select * from students where name like '%${q}%'`;
+    conn.query(sql, function (err,data){
+        if (err){
             res.send("404 not found");
         }else{
             res.send(data);
         }
     })
 });
-// search by class name
-app.get("/search-by-classname",function (req,res) {
-    const q = req.query.q;
-    const sql = `select * from students where cid in (select cid from classes where name like '%${q}%')`;
-    conn.query(sql,function (err,data) {
-        if(err){
+app.get("/student-by-classname",function (req,res){
+    const cn = req.query.cn;
+    const sql = `select * from students where cid in (select cid from classes where name like '%${cn}%')`;
+    conn.query(sql, function (err,data){
+        if (err){
             res.send("404 not found");
         }else{
             res.send(data);
@@ -85,33 +82,32 @@ app.get("/search-by-classname",function (req,res) {
     })
 });
 // get 1 student by sid
-app.get("/detail-student",function (req,res) {
+app.get("/detail-student",function (req,res){
     const sid = req.query.sid;
-    const sql = `select * from students where sid= ${sid}`;
-    conn.query(sql,function (err,data) {
-        if(err){
+    const sql = `select * from students where sid = ${sid}`;
+    conn.query(sql, function (err,data){
+        if (err){
             res.status(403).send("Error");
-        }else if(data.length > 0){
+        }else if (data.length > 0){
             res.send(data[0]);
         }else{
             res.status(404).send("404 not found");
         }
     })
 });
-
+// liet ke sinh vien
 app.get("/student",function (req,res){
-    //liet ke sinh vien
-    res.send ("Student with GET")
+    res.send("Student with GET");
 })
+// them sinh vien
 app.post("/student",function (req,res){
-    //them 1 sv
-    res.send ("Student with POST")
+    res.send("Student with POST");
 })
+// update sinh vien
 app.put("/student",function (req,res){
-    //update sv
-    res.send ("Student with PUT")
+    res.send("Student with PUT");
 })
+// xoa sinh vien
 app.delete("/student",function (req,res){
-    //delete sv
-    res.send ("Student with DELETE")
+    res.send("Student with DELETE");
 })
